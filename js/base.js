ee2b729cd8f232acc44851704fada1403e7f3a15
xishuiinsz <xishuiofchina@163.com>
2019-01-10 14:39:38 +0800

    在base.js中新增了getIndexMenu,getMoneyCtrl

--- js/base.js
+++ js/base.js
@@ -1,17 +1,51 @@
-// 创建一个构造函数
-var MMB = function(){
 
-}
-// 公共APiurl 公共属性 本地内网ip
-// MMB.prototype.baseUrl = 'http://localhost:9090';
-// 公共的外网ip
-MMB.prototype.baseUrl = 'http://47.52.242.30:9090';
-// 公共的函数
-MMB.prototype.getQueryString = function (name){
-    var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
-    var r = window.location.search.substr(1).match(reg);
-    if (r != null) {
-        return decodeURI(r[2]);
+(function (w) {
+    var MMB = function () {
+
+
+
     }
-    return null;
-}
+
+    // 静态属性,用来获取当前根域名:端口
+    MMB.domain = "http://127.0.0.1:9090";
+
+    /**
+     * 注意:此方法是静态方法,直接使用构造函数来调用,无须实例化.
+     * URL解码方法为:decodeURI
+     * 从URL中获取指定key的值,如http://xxxx?name=bulin,传入key,输出bulin
+     * @param {*} key 参数:字符串
+     * @returns JSON格式
+     */
+    MMB.getUrlParam = function (key) {
+        var reg = new RegExp("(^|&)" + key + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+        var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+        if (r != null) return decodeURI(r[2]); return null; //返回参数值
+    }
+
+    /**
+      * 用来获取首页菜单栏数据
+      * @param {*} 无
+      * @returns JSON格式
+      */
+    MMB.prototype.getIndexMenu = function () {
+        return axios(`${MMB.domain}/api/getindexmenu`).then(res => {
+            return res.data;
+            console.log(res);
+        })
+    }
+
+
+    /**
+      * 用来获取折扣商品的列表信息
+      * @param {*} 无
+      * @returns JSON格式
+      */
+    MMB.prototype.getMoneyCtrl = function () {
+        return axios(`${MMB.domain}/api/getmoneyctrl`).then(res => {
+            return res.data;
+            console.log(res);
+        })
+    }
+
+    w.MMB = MMB;
+})(window);
